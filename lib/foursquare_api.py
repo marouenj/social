f452d72493d6f2d6f78099f5f5df0b054a6d490b
Chaima Dhahri <chaima.aldhahri@gmail.com>
2016-11-27 21:38:52 +0900

    Add other metrics for foursquare

--- lib/foursquare_api.py
+++ lib/foursquare_api.py
@@ -1,9 +1,6 @@
 import json, requests, time
 
-def user(token, user_id):
-  url = 'https://api.foursquare.com/v2/users/' + user_id
-  params = {'oauth_token': token, 'v':'20161116'}
-
+def get(url, params, user_id):
   while True:
     r = requests.get(url, params=params)
     if r.status_code == 403:
@@ -14,3 +11,53 @@ def user(token, user_id):
       return None
     else:
       return json.loads(r.text)
+
+def user(token, user_id):
+  url = 'https://api.foursquare.com/v2/users/' + user_id
+  params = {'oauth_token': token, 'v':'20161116'}
+  return get(url, params, user_id)
+
+# only self can be used as user_id for now
+def user_checkins(token, user_id):
+  url = 'https://api.foursquare.com/v2/users/' + user_id + '/checkins'
+  params = {'oauth_token': token, 'limit':'250', 'sort':'newestfirst', 'v':'20161116'}
+  return get(url, params, user_id)
+
+def user_mayorships(token, user_id):
+  url = 'https://api.foursquare.com/v2/users/' + user_id + '/mayorships'
+  params = {'oauth_token': token, 'v':'20161116'}
+  return get(url, params, user_id)
+
+def user_venuelikes(token, user_id):
+  url = 'https://api.foursquare.com/v2/users/' + user_id + '/venuelikes'
+  params = {'oauth_token': token, 'limit':'100', 'v':'20161116'}
+  return get(url, params, user_id)
+
+def user_tips(token, user_id):
+  url = 'https://api.foursquare.com/v2/lists/' + user_id + '/tips'
+  params = {'oauth_token': token, 'limit':'200', 'sort':'recent', 'v':'20161116'}
+  return get(url, params, user_id)
+
+# returns 404
+def user_tastes(token, user_id):
+  url = 'https://api.foursquare.com/v2/users/' + user_id + '/tastes'
+  params = {'oauth_token': token, 'v':'20161116'}
+  return get(url, params, user_id)
+
+def parse_todo_count(user):
+  groups = user['lists']['groups']
+  for group in groups:
+    if group['type'] == 'yours':
+      for item in group['items']:
+        if item['type'] == 'todos':
+          return item['listItems']['count']
+  return None
+
+def parse_venuelikes_count(user):
+  groups = user['lists']['groups']
+  for group in groups:
+    if group['type'] == 'yours':
+      for item in group['items']:
+        if item['type'] == 'likes':
+          return item['listItems']['count']
+  return None
--- src/cross-osn-crawler/track_foursquare.py
+++ src/cross-osn-crawler/track_foursquare.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 import datetime
+#import json
+
 import config, people, foursquare_api
 
 token = config.load_token('/vagrant/token/foursquare.json')
@@ -12,6 +14,10 @@ today = str(datetime.date.today())
 i = 0
 for k, user in users.items():
   i += 1
+
+  if i > 1:
+    break
+
   print(i)
   foursquare = user['foursquare']
   if 'id' not in foursquare:
@@ -19,13 +25,47 @@ for k, user in users.items():
   user_id = foursquare['id']
   if user_id is None:
     continue
-  remote_user = fsq_api.user(token, user_id)
+
+  remote_user = foursquare_api.user(token, user_id)
   if remote_user is None:
     continue
+  #with open('./user.json', 'w') as file:
+  #  json.dump(remote_user, file, sort_keys=True)
+
+  #remote = foursquare_api.user_mayorships(token, user_id)
+  #if remote is None:
+  #  continue
+  #with open('./mayorships.json', 'w') as file:
+  #  json.dump(remote, file, sort_keys=True)
+
+  #remote = foursquare_api.user_venuelikes(token, user_id)
+  #if remote is None:
+  #  continue
+  #with open('./venuelikes.json', 'w') as file:
+  #  json.dump(remote, file, sort_keys=True)
+
+  #remote_tips = foursquare_api.user_tips(token, user_id)
+  #if remote_tips is None:
+  #  continue
+  #with open('./tips.json', 'w') as file:
+  #  json.dump(remote_tips, file, sort_keys=True)
+
   if '_activity' not in foursquare:
     foursquare['_activity'] = {}
+
   remote_user = remote_user['response']['user']
-  activity = {'friends_count':remote_user['friends']['count'],'tips_count':remote_user['tips']['count'],'checkins_count':remote_user['checkins']['count']}
+
+  activity = {
+    'bio':remote_user['bio'],
+    'friends_count':remote_user['friends']['count'],
+    'tips_count':remote_user['tips']['count'],
+    'checkins_count':remote_user['checkins']['count'],
+    'lists_count':remote_user['lists']['count'],
+    'mayorships_count':remote_user['mayorships']['count'],
+    'todo_count':foursquare_api.parse_todo_count(remote_user),
+    'venuelikes_count':foursquare_api.parse_venuelikes_count(remote_user)
+  }
+
   foursquare['_activity'][today] = activity
 
 people.save(users, './people.json')
